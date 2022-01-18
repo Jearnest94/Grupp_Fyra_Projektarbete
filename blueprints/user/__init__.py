33166489fe1a87c671e43d3adc60ddd83890b567
Fajit <49131566+JaybirdDotSe@users.noreply.github.com>
2022-01-18 22:53:34 +0100

    Added chat and messages html and to basetemplate navbar

--- blueprints/user/__init__.py
+++ blueprints/user/__init__.py
@@ -3,6 +3,16 @@ from flask import Blueprint, render_template
 bp_user = Blueprint('bp_user', __name__)
 
 
-@bp_user.get('/user')
-def user_get():
-    return render_template("user.html")
+@bp_user.get('/profile')
+def profile_get():
+    return render_template("profile.html")
+
+
+@bp_user.get('/chat')
+def chat_get():
+    return render_template('chat.html')
+
+
+@bp_user.get('/messages')
+def messages_get():
+    return render_template('messages.html')
