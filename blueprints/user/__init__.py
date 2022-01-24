60eb6ff9c976c6e99feca9337ee73dcca753228b
Raamfox <rami.okal@student.kyh.se>
2022-01-24 17:30:13 +0100

    added chat html and chat css

--- blueprints/user/__init__.py
+++ blueprints/user/__init__.py
@@ -39,7 +39,8 @@ def chat_get():
 
 @bp_user.get('/messages')
 def messages_get():
-    return render_template('messages.html', name=current_user.name, email=current_user.email, title=Message.query.first().title, content=Message.query.first().content, fromuser=Message.query.first().sender, timestamp=Message.query.first().timestamp)
+    return render_template('messages.html', name=current_user.name, email=current_user.email, title=Message.query.first().title,
+                           content=Message.query.first().content, fromuser=Message.query.first().sender, timestamp=Message.query.first().timestamp)
 
 
 @bp_user.post('/messages')
